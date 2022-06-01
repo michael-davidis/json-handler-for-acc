56d33d1f07be4c558baeaeecf881e82fb3afc16e
Michael Davidis <mdavidis@cluster.gr>
2022-06-01 18:50:19 +0300

    JHA-1: Create json form controller.

---
+++ src/Controller/JsonFormController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JsonFormController extends AbstractController
+{
+    /**
+     * @Route("/json/form", name="app_json_form")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/JsonFormController.php',
+        ]);
+    }
+}
