a43c332cab0577d2c49e63325d00b25c4390172d
Michael Davidis <mdavidis@cluster.gr>
2022-06-01 21:56:11 +0300

    JHA-1: Render first iteration of form on frontend.

--- src/Controller/JsonFormController.php
+++ src/Controller/JsonFormController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\JsonInputForm;
+use App\Form\JsonInputFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JsonFormController extends AbstractController
@@ -11,11 +14,16 @@ class JsonFormController extends AbstractController
     /**
      * @Route("/", name="app_json_form")
      */
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/JsonFormController.php',
+    public function index(Request $request): Response
+    {   
+        $jsonForm = new JsonInputForm();
+
+        $form = $this->createForm( JsonInputFormType::class, $jsonForm);
+
+        return $this->renderForm('json.input.form.html.twig', [
+            'form' => $form,
         ]);
+
     }
+
 }
